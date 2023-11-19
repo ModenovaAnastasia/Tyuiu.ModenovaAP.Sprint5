2b8035ca200b47b5369f25b50fe2f86ea239bfc7
Tyuiu.ModenovaAP@outlook.com <fanriall@mail.ru>
2023-11-19 19:12:15 +0500

    Task1

---
+++ Tyuiu.ModenovaAP.Sprint5.Task1.V18.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.ModenovaAP.Sprint5.Task1.V18.Lib;
+
+namespace Tyuiu.ModenovaAP.Sprint5.Task1.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\1\source\repos\Tyuiu.ModenovaAP.Sprint5\Tyuiu.ModenovaAP.Sprint5.Task1.V18\bin\Debug\OutPutFileTask1.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.ModenovaAP.Sprint5.Task1.V18/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ModenovaAP.Sprint5.Task1.V18.Lib;
+
+namespace Tyuiu.ModenovaAP.Sprint5.Task1.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Моденова А. П. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Моденова Анастасия Павловна | ИИПб-23-3                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция, F(x) = 3x+2 - (2*x-x)/(cos(x)+1)(произвести табулирование)*");
+            Console.WriteLine("* f(x) на заданном диапазоне [-5;5] с шагом 1ю Произвести проверку деле - *");
+            Console.WriteLine("* ния на ноль. При деление на ноль вернуть значение 0. Результат сохранить*");
+            Console.WriteLine("* в текстовый файл OutPutFileTask1.txt и вывести на консоль в таблицу.    *");
+            Console.WriteLine("* Значение округлить до двух знаков после запятой.*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string result = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + result);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
